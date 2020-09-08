2fd2ad688a479b2b3c121235adf1aa9bf0c44bde
1224192567@qq.com <1224192567@qq.com>
2020-09-08 10:56:43 +0800

    first commit

---
+++ HCLGeneralUI/Classes/HCLObject.h
@@ -0,0 +1,17 @@
+//
+//  HCLObject.h
+//  SuperCard
+//
+//  Created by Cunlong Huo on 2020/9/8.
+//  Copyright © 2020 G-World. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HCLObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HCLGeneralUI/Classes/HCLObject.m
@@ -0,0 +1,20 @@
+//
+//  HCLObject.m
+//  SuperCard
+//
+//  Created by Cunlong Huo on 2020/9/8.
+//  Copyright © 2020 G-World. All rights reserved.
+//
+
+#import "HCLObject.h"
+
+@implementation HCLObject
+
+- (instancetype)init {
+    if (self = [super init]) {
+        NSLog(@"成功阿拉了啦");
+    }
+    return self;
+}
+
+@end
